731f956ab00a447b2d09ecd908a4d3d057079716
sonofice <sonoficewolff@gmail.com>
2021-02-19 00:05:14 +0100

    creation of dictionary's

---
+++ main.py
@@ -0,0 +1,29 @@
+#imports
+import random
+
+# Suits dictionary 
+suits = {
+    0: 'Clubs',
+    1: 'Diamonds',
+    2: 'Hearts',
+    3: 'Spades'
+}
+
+# Cards dictionary
+cards = {
+    0: 'Ace',
+    1: '2',
+    2: '3',
+    3: '4',
+    4: '5',
+    5: '6',
+    6: '7',
+    7: '8',
+    8: '9',
+    9: '10',
+    10: 'Jack',
+    11: 'Queen',
+    12: 'King'
+}
+
+def draw_cards():
