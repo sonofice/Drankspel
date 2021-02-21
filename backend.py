04d220544b1db3a151baa6430858981d30f7e424
sonofice <sonoficewolff@gmail.com>
2021-02-21 01:20:34 +0100

    Database creation

---
+++ backend.py
@@ -0,0 +1,36 @@
+# Imports
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect("playerdb.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE if not exits Playerdata (Player TEXT, Score INTEGER)")
+    conn.commit()
+    conn.close()
+
+def insert(score):
+    conn = sqlite3.conncect("playerdb.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Playerdata VALUES(?,?)", (Player, Score))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("playerdb.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Playerdata")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(score):
+    conn = sqlite3.connect("playerdb.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM score Where Player=?", (Player))
+
+def update(score):
+    conn = sqlite3.connect("playerdb.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE Playerdata SET Score=? WHERE Player=?", (Score, Player))
+    conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -1,55 +1,21 @@
-#imports
-import random
+# Imports
+from random import randint
 
-# Suits dictionary 
-suits = {
-    0: 'Clubs',
-    1: 'Diamonds',
-    2: 'Hearts',
-    3: 'Spades'
-}
 
-# Cards dictionary
-cards = {
-    0: 'Ace',
-    1: '2',
-    2: '3',
-    3: '4',
-    4: '5',
-    5: '6',
-    6: '7',
-    7: '8',
-    8: '9',
-    9: '10',
-    10: 'Jack',
-    11: 'Queen',
-    12: 'King'
-}
+# Lists with the Suits and the Cards
+Suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+Cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-def draw_cards(num_of_cards, list_dealt=[]):
-    for z in range(num_of_cards):
-        x = randint(0,3)
-        y = randint(0,12)
-        mycard = "{0} of {1}".format(cards[y],suits[x])
-        if mycard not in list_dealt:
-            list_dealt.append(mycard)
-        else:
-            num_of_cards = num_of_cards - z
-            return draw_cards(num_of_cards,list_dealt)
-            
-    return list_dealt
+# Full card deck
+deck = []
 
-# start of script
-# Enter number of cards the player wants to get from the deck
-default = 1
-mydraw = draw_cards(input("How many cards do you want to draw?: ")) #call the function with the number of cards you want. 
-if not input:
-    mydraw = default
+# Function to create the cards
+def create_cards():
+    for suit in Suits:
+        for card in Cards:
+            deck.append([suit + " " + card])
+create_cards()
+print(*deck)
 
-i = 0
-for x in mydraw:
-    i += 1
-    if i == len(mydraw):
-        print("...And your last card is the {0}".format(str(x)))
-    else:
-        print("You got the {0}".format(str(x)))
+
+
